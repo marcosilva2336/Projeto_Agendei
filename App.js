fe6fe8fa9910dfb2c51bc890bb1a83ff50991679
Marcos Silva <joao1245marcos@outlook.com>
2024-10-16 23:55:38 -0300

    tela de serviço

--- App.js
+++ App.js
@@ -4,10 +4,11 @@ import AbaHome from './src/screens/abahome/abahome.jsx';
 import AbaCalendar from './src/screens/abacalendar/abacalendar.jsx';
 import AbaProfile from './src/screens/abaprofile/abaprofile.jsx';
 import Main from './src/screens/main/main.jsx';
+import Services from './src/screens/services/services.jsx';
 function App() {
   return (
     <>
-      <Main />
+      <Services />
     </>
   );
 }
